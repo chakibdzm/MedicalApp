436a22329835ef4fa7fa516f4eac8905f685ae08
oihid <c.ouchene@esi-sba.dz>
2022-10-25 09:28:11 +0100

    first onboarding

--- lib/Screens/Onboarding/Onboarding.dart
+++ lib/Screens/Onboarding/Onboarding.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:medicalapp/size_config.dart';
 class Onboarding extends StatefulWidget {
   const Onboarding({Key? key}) : super(key: key);
 
@@ -7,8 +9,80 @@ class Onboarding extends StatefulWidget {
 }
 
 class _OnboardingState extends State<Onboarding> {
+  final _controller = PageController();
+
   @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
   Widget build(BuildContext context) {
-    return Container();
+    SizeConfig().init(context);
+
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+          child: PageView(
+            controller: _controller,
+            children: [
+              Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: getHeight(136),),
+                    Center(child: SvgPicture.asset('assets/illustrations/8578 1.svg'),),
+                    SizedBox(height: getHeight(102),),
+                    Center(child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Estés donde estés',
+                        style: TextStyle(
+                          fontSize: getHeight(22),
+                          fontWeight: FontWeight.w700,
+                          color: Colors.black,
+                        ),
+                        ),
+                        SizedBox(height: getHeight(10),),
+                        Text('Lorem ipsum dolor sit \namet, consectetur \nadipiscing elit. Ut.',
+                          style: TextStyle(
+                            fontSize: getHeight(16),
+                            fontWeight: FontWeight.w400,
+                            color: Colors.black,
+                          ),
+                        )
+                      ],
+                    ),),
+                    SizedBox(height: getHeight(30),),
+                    Center(
+                      child: InkWell(child: Container(
+                        height: getHeight(50),
+                        width: getWidth(323),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(getHeight(30),
+                          ),
+                          color: Color(0xFF007AFE),
+
+                        ),
+                        child: Center(
+                          child: Text('Go Next !',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600,
+                            fontSize: getHeight(16),
+                          ),),
+                        ),
+                      ),),
+                    )
+                    
+                  ],
+                ),
+
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
